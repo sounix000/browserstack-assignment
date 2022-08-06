eedca223ae153a4e95453bf8c81192204467a3a7
Souvik Sarkar <sosarkar@redhat.com>
2022-08-06 21:55:30 +0530

    Update

--- browserstack_script.py
+++ browserstack_script.py
@@ -1,45 +1,79 @@
-from selenium.webdriver import Chrome
-import time # The function time.sleep() helps mask the scripted bot behavior.
-
-# Helper function to mimic slow typing by a human
-def slow_typing(element, text):
-    for character in text: 
-        element.send_keys(character)
-        time.sleep(0.3)
-
-# Email registered with Amazon
-EMAIL_ID = "<email_registered_with_Amazon>"
-
-# Read password from a text file and add the file to .gitignore
-# Do not hardcode the password for your Amazon account.
-password = ""
-with open('password.txt', 'r') as password_file:
-    password = password_file.read().replace('\n', '')
-
-# Open browser and go to sign in page
-browser = Chrome()
-browser.get('https://amazon.in/')
-time.sleep(2)
-sign_in_button = browser.find_element_by_xpath('//*[@id="nav-link-accountList"]/span')
-sign_in_button.click()
-time.sleep(2)
-
-# Assumption: There are no two-factor authentication enabled
-# Enter the sign in credentials
-username_textbox = browser.find_element_by_id("ap_email")
-slow_typing(username_textbox, EMAIL_ID)
-time.sleep(2)
-
-continue_button = browser.find_element_by_id("continue")
-continue_button.submit()
-time.sleep(2)
-
-password_textbox = browser.find_element_by_id("ap_password")
-slow_typing(password_textbox, password)
-time.sleep(2)
-
-sign_in_button = browser.find_element_by_id("auth-signin-button-announce")
-sign_in_button.submit()
-time.sleep(20)
-
-browser.close()
+from dotenv import load_dotenv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from threading import Thread
+# This array 'capabilities' defines the capabilities browser, device and OS combinations where the test will run
+load_dotenv()
+BUILD_NAME = "browserstack-build-1"
+capabilities = [
+    {
+        "browserName": "chrome",
+        "browserVersion": "103.0",
+        "os": "Windows",
+        "osVersion": "11",
+        "sessionName": "Parallel Test 1",  # test name
+        "buildName": BUILD_NAME,  # Your tests will be organized within this build
+    },
+    {
+        "browserName": "firefox",
+        "browserVersion": "102.0",
+        "os": "Windows",
+        "osVersion": "10",
+        "sessionName": "Parallel Test 2",
+        "buildName": BUILD_NAME,
+    },
+    {
+        "browserName": "safari",
+        "browserVersion": "14.1",
+        "os": "OS X",
+        "osVersion": "Big Sur",
+        "sessionName": "Parallel Test 3",
+        "buildName": BUILD_NAME,
+    },
+]
+def get_browser_option(browser):
+    switcher = {
+        "chrome": ChromeOptions(),
+        "firefox": FirefoxOptions(),
+        "edge": EdgeOptions(),
+        "safari": SafariOptions(),
+    }
+    return switcher.get(browser, ChromeOptions())
+# run_session function searches for 'BrowserStack' on duckduckgo.com
+def run_session(cap):
+    cap["userName"] = os.environ.get("BROWSERSTACK_USERNAME")
+    cap["accessKey"] = os.environ.get("BROWSERSTACK_ACCESS_KEY")
+    options = get_browser_option(cap["browserName"].lower())
+    options.set_capability("browserName", cap["browserName"].lower())
+    options.set_capability("bstack:options", cap)
+    driver = webdriver.Remote(
+        command_executor="https://hub.browserstack.com/wd/hub", options=options
+    )
+    driver.get("https://www.duckduckgo.com")
+    if not "DuckDuckGo" in driver.title:
+        raise Exception("Unable to load duckduckgo page!")
+    elem = driver.find_element(By.NAME, "q")
+    elem.send_keys("BrowserStack")
+    elem.submit()
+    try:
+        WebDriverWait(driver, 5).until(EC.title_contains("BrowserStack"))
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Title matched!"}}'
+        )
+    except TimeoutException:
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Title not matched"}}'
+        )
+    print(driver.title)
+    driver.quit()
+# The Thread function takes run_session function and each set of capability from the caps array as an argument to run each session parallelly
+for cap in capabilities:
+    Thread(target=run_session, args=(cap,)).start()
