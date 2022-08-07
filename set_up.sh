25426686cee0f511a90eb3759bf1c1304f9cc578
Souvik Sarkar <sosarkar@redhat.com>
2022-08-07 17:22:03 +0530

    Update

---
+++ set_up.sh
@@ -0,0 +1,23 @@
+# This script does the following:
+# - sets up a Python virtual environment in the Ubuntu runner for GitHub Actions
+# - installs all prerequisites for running the `browserstack_script.py` file in the Ubuntu runner for GitHub Actions 
+# the file `browserstack_script.py` on a Ubuntu runner.  
+#
+# You can also run this script to set up your environment and dependencies.
+#
+# Check out the list of preinstalled packages for Ubuntu 22.04: https://github.com/actions/virtual-environments/blob/main/images/linux/Ubuntu2204-Readme.md
+
+# Set up and activate a Python virtual environment in the project directory.
+# Assumption: You have already checked out the project repository from GitHub.
+cd browserstack-assignment/
+python -m venv browserstack
+source browserstack/bin/activate
+
+# Install selenium v 4.1.0
+python -m pip install selenium==4.1.0
+
+# Install python-dotenv package for handling environment variables from the test script
+python -m pip install python-dotenv
+
+# Note: Because the selenium webdrivers invoke headless Chrome and Firefox in the remote BrowserStack Cloud, 
+# the Ubuntu runner need not have the specific versions of Chrome and Firefox drivers as mentioned in `browserstack_script.py`.
